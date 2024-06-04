ebb36d435785d748d30bc3afd6970cc867cc09f9
QAwgie <159956314+QAwgie@users.noreply.github.com>
2024-06-04 15:24:00 +0200

    Create OddEvenchecker.java

---
+++ OddEvenchecker.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class OddEvenChecker {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an numberr: ");
+        int number = scanner.nextInt();
+
+        String result = (number % 2 == 0) ? "even" : "odd";
+        System.out.println(number + " is " + result + ".");
+    }
+}

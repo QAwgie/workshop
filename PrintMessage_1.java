884e677e2edcaa1ac7ec8db8e5d3112528e7ba09
QAwgie <159956314+QAwgie@users.noreply.github.com>
2024-06-04 15:44:53 +0200

    Create PrintMessage_1.java

---
+++ PrintMessage_1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+class PrintMessage_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine());
+    }
+}

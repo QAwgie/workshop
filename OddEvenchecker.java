74ddc343f86807a1e70eb0b695cc9ef4ce519cdd
QAwgie <159956314+QAwgie@users.noreply.github.com>
2024-06-05 14:41:50 +0200

    Update OddEvenchecker.java

--- OddEvenchecker.java
+++ OddEvenchecker.java
@@ -2,13 +2,22 @@ import java.util.Scanner;
 
 public class OddEvenChecker {
 
+    // Even/Odd checker method
+    public static String checkOddOrEven(int number) {
+        String result = (number % 2 == 0) ? "even" : "odd";
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
 
-        String result = (number % 2 == 0) ? "even" : "odd";
-        System.out.println(number + " is " + result + ".");
+        // Calling the method
+        String result = checkOddOrEven(number);
+
+        // Print results
+        System.out.println("Number " + number + " is " + result + ".");
     }
 }

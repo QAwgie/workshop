a5b55cdab56f628cd8d9bfee02c390d0a7ddf723
QAwgie <159956314+QAwgie@users.noreply.github.com>
2024-06-05 14:55:02 +0200

    Update PrintMessage_2.java

--- PrintMessage_2.java
+++ PrintMessage_2.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
 
 class PrintMessage_2 {
-    public static void main(String[] args) {
+
+    // Method for reading and printing a message
+    public static void readAndPrintMessage() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a message: ");
         String message = scanner.nextLine();
         System.out.println("Your entered message is: " + '"' + message + '"');
+    }
+        // Calling the method
+    public static void main(String[] args) {
+        readAndPrintMessage();
     }
 }
